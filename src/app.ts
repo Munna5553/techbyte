16fa8c2c24282b6de3b51cb12bfd7d4d3f97a6b6
Munna5553 <shauryakumar42040er@gmail.com>
2024-06-16 21:08:58 +0530

    add express, morgan and cors with middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
+const app = express();
+
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(express.static("public"));
+
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true
+}));
+app.use(morgan("dev"));
+
+export { app };
